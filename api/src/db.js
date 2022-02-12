cec0b2dbbff1f88632a75337257170daea13ae31
Jhon.Crow <juancho.cuervo@hotmail.com>
2022-02-12 11:50:44 -0500

    models

--- api/src/db.js
+++ api/src/db.js
@@ -30,10 +30,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Pokemon } = sequelize.models;
+const { country, activity } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+country.hasMany(activity);
+activity.hasMany(country)
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,52 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
-    name: {
+    Nombre: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    ID: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+    },
+    Bandera: {
+      allowNull: false,
+    },
+    Continente: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    Subregion: {
+      type: DataTypes.STRING,
+    },
+    Area: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    Población: {
+      type: DataTypes.INTEGER
+    },
+  });
+
+  sequelize.define('activity', {
+    ID: {
+      type: DataTypes.INTEGER
+    },
+    Nombre: {
+      type: DataTypes.STRING
+    },
+    Dificultad: {
+      type: DataTypes.INTEGER
+    },
+    Duracion: {
+      type: DataTypes.INTEGER
+    },
+    Temporada: {
+      type: DataTypes.STRING
     },
   });
 };
