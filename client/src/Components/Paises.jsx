954921f512890eeb387b2ec3d10c0f1ac0fb9185
Jhon.Crow <juancho.cuervo@hotmail.com>
2022-02-22 09:49:27 -0500

    ruta detalle actualizada

--- client/src/Components/Paises.jsx
+++ client/src/Components/Paises.jsx
@@ -22,17 +22,28 @@ export default function DetailPais(props){
             <div>
                 <h2>{pais.nombre}</h2>
 
-                Bandera={pais.bandera}
-                <p>
-                Continente={pais.continente}
-                Capital={pais.capital}
-                Subregion={pais.subregion}
-                Area={pais.area}
-                Poblacion={pais.poblacion}
-                </p> 
+                <img src={pais.bandera} alt='not found'/>
+
+                <h3>Continente: {pais.continente}</h3>
+                <h3>Capital: {pais.capital}</h3>
+                <h3>Subregion: {pais.subregion}</h3>
+                <h3>Area: {pais.area}</h3>
+                <h3>Población:{pais.poblacion}</h3>          
 
                 <div>
-                Actividades={pais.activities?.map(a =>( a.nombre))}
+                    <h4>
+                    Actividades
+                    {pais.activities?.map(a =>(
+                        <div>
+                        <h5>Nombre: {a.nombre}</h5>
+                        <h5>Dificultad: {a.dificultad}</h5>
+                        <h5>Duración: {a.duracion}</h5>
+                        <h5>Temporada: {a.temporada}</h5>
+                        </div>
+                    
+                    ))}
+                    </h4>
+               
                  </div>
     
             </div>
